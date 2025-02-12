b64d9d9231ef30c18c5c0e2962fe81b47c9e625b
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-02-12 13:16:52 +0530

    d

--- src/common/notification_emitter/notification.event.ts
+++ src/common/notification_emitter/notification.event.ts
@@ -4,15 +4,15 @@
  * MIT license that can be found in the LICENSE file.
  */
 
-import {Inject, Logger, Injectable} from "@nestjs/common";
-import {OnEvent} from "@nestjs/event-emitter";
+import { Inject, Logger, Injectable } from "@nestjs/common";
+import { OnEvent } from "@nestjs/event-emitter";
 import * as OneSignal from "onesignal-node";
-import {ConfigService} from "@nestjs/config";
-import {CreateNotificationBody} from "onesignal-node/lib/types";
-import {getMessaging, Messaging} from "firebase-admin/messaging";
-import {UserService} from "../../api/user_modules/user/user.service";
-import {UserDeviceService} from "../../api/user_modules/user_device/user_device.service";
-import {PushTopics} from "../../core/utils/enums";
+import { ConfigService } from "@nestjs/config";
+import { CreateNotificationBody } from "onesignal-node/lib/types";
+import { getMessaging, Messaging } from "firebase-admin/messaging";
+import { UserService } from "../../api/user_modules/user/user.service";
+import { UserDeviceService } from "../../api/user_modules/user_device/user_device.service";
+import { PushTopics } from "../../core/utils/enums";
 
 //
 export class NotificationData {
@@ -69,7 +69,7 @@ export class NotificationEvent {
         if (!this.onesignalClient) {
             return;
         }
-        await this.onesignalClient.editDevice(token, {"tags": {[topic]: true}});
+        await this.onesignalClient.editDevice(token, { "tags": { [topic]: true } });
     }
 
     @OnEvent("topic.fcm")
@@ -90,40 +90,42 @@ export class NotificationEvent {
         }
     }
 
-
-    @OnEvent("send.all.active")
+    @OnEvent('send.all.active')
     async sendToAllActiveUsers(title: string, body: string) {
         if (this.isFirebaseFcmEnabled) {
             try {
+                // Sending notification to Android topic
                 await this.messaging.send({
-                    topic: PushTopics.AdminAndroid,
+                    topic: 'AdminAndroid', // Make sure this topic is correctly subscribed by users
                     notification: { body, title },
-                    android: { priority: "high" },
+                    android: { priority: 'high' },
                     apns: { payload: { aps: { contentAvailable: true } } },
                 });
 
+                // Sending notification to iOS topic
                 await this.messaging.send({
-                    topic: PushTopics.AdminIos,
+                    topic: 'AdminIos', // Make sure this topic is correctly subscribed by users
                     notification: { body, title },
-                    android: { priority: "high" },
+                    android: { priority: 'high' },
                     apns: { payload: { aps: { contentAvailable: true } } },
                 });
+
             } catch (err) {
-                console.log(err);
+                console.error('Error sending Firebase notification:', err);
             }
         }
 
         if (this.isOneSignalEnabled) {
             const notification: CreateNotificationBody = {
-                "included_segments": ["Active Users", "Subscribed Users"],
-                "priority": 10,
-                headings: { "en": title },
-                "contents": { "en": body }
+                included_segments: ['Active Users', 'Subscribed Users'],
+                priority: 10,
+                headings: { en: title },
+                contents: { en: body },
             };
 
             this.onesignalClient.createNotification(notification)
-                .then(response => { })
-                .catch(e => { console.log(e); });
+                .then(response => console.log('OneSignal notification sent:', response))
+                .catch(err => console.error('OneSignal error:', err));
         }
     }
 
@@ -223,7 +225,7 @@ export class NotificationEvent {
             ],
             "priority": 10,
             "include_player_ids": tokens,
-            headings: {"en": event.title},
+            headings: { "en": event.title },
             "contents": {
                 "en": event.body
             },
