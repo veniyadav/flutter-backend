c5ff26707b47d92f335bdf11391381742c0aca7c
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-02-15 19:06:49 +0530

    d

--- src/index.ts
+++ src/index.ts
@@ -73,7 +73,7 @@ async function bootstrap() {
   app.useStaticAssets(join(root.path, "public", "media"));
   await app.listen(port);
 
-  console.log("app run on port " + port);
+  console.log(`app run on port${port}`);
 }
 
 bootstrap();
